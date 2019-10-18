3ea24c7163b9581e77f813095cef876dbb5e6401
maxbender <mbender4242@gmail.com>
2019-10-18 14:09:12 -0500

    Added Scatter Plot Matrix to R Script

--- BasketballAnalysisRScript.R
+++ BasketballAnalysisRScript.R
@@ -1,11 +1,16 @@
-library(lattice)
-library(ggplot2)
-
+#Read CSV Files from Git Directory
 df_awards = read.csv("basketball_awards_players.csv")
 df_hof = read.csv("basketball_hof.csv")
 df_players = read.csv("basketball_players.csv")
 
+#See first 6 rows of dataframes
 head(df_hof)
 head(df_players)
 
+#Merge HOF and Players Data on playerID/hofID, inlcuding all rows from both dataframes
 df_hof_players = merge(df_players, df_hof, by.x = c("playerID"), by.y = c("hofID"), all = T)
+names(df_hof_players)
+head(df_hof_players)
+
+#Create Scatter Plot Matrix with Numerical Stats (this will take a couple minutes to run!!)
+pairs(df_hof_players[8:16])
