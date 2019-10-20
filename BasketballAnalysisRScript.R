2359f27c3933a824a481a2e632fb37a9bf3e28e1
Ryan Davidson <rdd2@rice.edu>
2019-10-20 15:06:13 -0500

    Updated script and added HOF binary to csv

--- BasketballAnalysisRScript.R
+++ BasketballAnalysisRScript.R
@@ -2,6 +2,7 @@
 df_awards = read.csv("basketball_awards_players.csv")
 df_hof = read.csv("basketball_hof.csv")
 df_players = read.csv("basketball_players.csv")
+df_careers = read.csv("basketball_players_careers.csv")
 
 #See first 6 rows of dataframes
 head(df_hof)
@@ -14,3 +15,11 @@ head(df_hof_players)
 
 #Create Scatter Plot Matrix with Numerical Stats (this will take a couple minutes to run!!)
 pairs(df_hof_players[8:16])
+
+#EDA for points, rebounds, and assists
+mean(df_careers$points)
+tapply(df_careers$points,INDEX=df_careers$HOF,mean)
+mean(df_careers$rebounds)
+tapply(df_careers$rebounds,INDEX=df_careers$HOF,mean)
+mean(df_careers$assists)
+tapply(df_careers$assists,INDEX=df_careers$HOF,mean)
